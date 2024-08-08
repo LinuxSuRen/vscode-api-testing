54496cd3a6041faf2e1d09dfc2fa3e80089f3eec
zhaoxiaojie <zhaoxiaojie@ailinking.com>
2024-08-08 08:46:33 +0000

    feat: support to start api-testing mock server

--- extension.js
+++ extension.js
@@ -8,8 +8,10 @@ const cp = require('child_process');
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const PROTO_PATH = __dirname +'/server.proto';
+const atestCodeLense = require('./api-testing.js');
+const atestCodeLenseMock = require('./api-testing-mock.js')
 const packageDefinition = protoLoader.loadSync(
-  PROTO_PATH, { 
+  PROTO_PATH, {
    keepCase: true,
    longs: String,
    enums: String,
@@ -45,49 +47,16 @@ function activate(context) {
 
 	const toggleLink = {
 		provideCodeLenses: function (document, token) {
-			if (document.lineAt(0).text !== "#!api-testing") {
-				return []
+			if (document.lineAt(0).text === "#!api-testing") {
+				return atestCodeLense(document, token)
+			} else if (document.lineAt(0).text === "#!api-testing-mock") {
+				return atestCodeLenseMock.codeLense(document, token)
 			}
-
-			// for the whole test suite
-			const range = new vscode.Range(0, 1, 10, 10)
-			const lens = new vscode.CodeLens(range, {
-				command: 'atest',
-				title: 'run suite'
-			})
-			const lensRunWith = new vscode.CodeLens(range, {
-				command: 'atest.runwith',
-				title: 'run suite with env'
-			})
-			let result = [lens, lensRunWith]
-
-			// for test cases
-			for (let i = 0; i < document.lineCount; i++) {
-				let nameAnchor = document.lineAt(i).text
-				nameAnchor = nameAnchor.trimLeft(' ', '')
-				if (nameAnchor.startsWith('- name: ')) {
-					let name = nameAnchor.replace('- name: ', '')
-					const range = new vscode.Range(i, 1, i, 10)
-					const testcaseLens = new vscode.CodeLens(range, {
-						command: 'atest',
-						title: 'run',
-						arguments: [name, "info"]
-					})
-					const testcaseDebugLens = new vscode.CodeLens(range, {
-						command: 'atest',
-						title: 'debug',
-						arguments: [name, "debug"]
-					})
-					result.push(testcaseLens, testcaseDebugLens)
-				}
-			}
-
-			return result
 		},
-		resolveCodeLens: function (code,token) {
+		resolveCodeLens: function (code, token) {
 			return code
 		}
-	  }
+	}
 
 	const codeLens = vscode.languages.registerCodeLensProvider({ language: 'yaml', scheme: 'file' }, toggleLink)
 	context.subscriptions.push(codeLens, statusBar)
@@ -124,14 +93,13 @@ function activate(context) {
 					apiConsole.appendLine(response.message);
 					apiConsole.appendLine(response.error);
 				}
-			 });
+			});
 		}  else {
 			let message = "YOUR-EXTENSION: Working folder not found, open a folder an try again" ;
 		
 			vscode.window.showErrorMessage(message);
 		}
 	})
-
 	
 	let atestRunWith = vscode.commands.registerCommand('atest.runwith', function(args) {
 		const dir = require('path').dirname(vscode.window.activeTextEditor.document.uri.fsPath)
@@ -219,7 +187,14 @@ function activate(context) {
 		});
 	})
 
-	context.subscriptions.push(atest, atestRunWith, atestSample);
+	let startMockServerCommand = vscode.commands.registerCommand('atest.startMock', function(args) {
+		if(vscode.workspace.workspaceFolders !== undefined) {
+			let filename = vscode.window.activeTextEditor.document.fileName
+			atestCodeLenseMock.startMock(filename)
+		}
+	})
+
+	context.subscriptions.push(atest, atestRunWith, atestSample, startMockServerCommand);
 
 	var which = require('which')
 	which('atest', { nothrow: true }).then((p) => {
@@ -227,7 +202,7 @@ function activate(context) {
 			startAtestServer()
 		} else {
 			vscode.window.showInformationMessage('Start to install API Testing.')
-			https.get("https://ghproxy.com/https://github.com/LinuxSuRen/api-testing/releases/latest/download/atest-linux-amd64.tar.gz", function(response) {
+			https.get("https://files.m.daocloud.io/github.com/LinuxSuRen/api-testing/releases/latest/download/atest-linux-amd64.tar.gz", function(response) {
 				const file = fs.createWriteStream('/tmp/atest.tar.gz');
 				response.pipe(file);
 				vscode.window.showInformationMessage('Receiving API Testing binary file.')
